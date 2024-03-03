8818f9cd00215e34c1a0bd9bf92ae6288c029c96
NelsonKinyanjui <nelsonkinyanjui246@gmail.com>
2024-03-03 10:02:26 +0300

    run for loop

--- assets/while-cd.js
+++ assets/while-cd.js
@@ -1,4 +1,23 @@
-/*
+
 const output = document.querySelector('.output');
 output.innerHTML= '';
- */
+
+let i = 10;
+
+do{
+    const para = document.createElement('p');
+    output.appendChild(para);
+    if (i === 10){
+        para.textContent =`countdown ${[i]}`;
+    }
+    else if (i === 0){
+        para.textContent = "Blast off!";
+    }
+    else {
+        para.textContent =[i];
+    }
+
+    i--
+
+}while(i > -1);
+ 
